d16363345f9b0a53ed3459ea7b6ab1cb7b0805e8
baker97-coder <baker.d.andrew@gmail.com>
2020-09-14 13:30:01 -0700

    cleaned up code

--- main.py
+++ main.py
@@ -34,8 +34,10 @@ def forward(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
-    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)),
+                   bd=1, relief=SUNKEN, anchor=E)
     status.grid(row=2, column=0, columnspan=3, sticky=W + E)
+
 
 def back(image_number):
     global my_label
